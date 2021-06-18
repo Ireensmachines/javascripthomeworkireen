f2d0ca57d39008bddaefe1e95a95c31d660cc09a
ireensmachines <ireen1981@hotmail.com>
2021-06-18 13:48:57 +0200

    eerste commit alles is af

---
+++ switchstatements.js
@@ -0,0 +1,16 @@
+const fruit = "Apples";
+
+switch (fruit){
+    case "Bananen" :
+        console.log("het zijn bananen");
+        break;
+    case "apples" :
+        console.log("het zijn appels!");
+        break;
+    case "citroenen" :
+        console.log("het zijn citroenen");
+        break;
+    default:
+       console.log("er is geen fruit gekozen");
+
+}
